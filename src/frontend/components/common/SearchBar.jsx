60109a75c733b1d533082c40597c892c7bfaa7aa
rohit-bahuguna <rohitbahuguna.4446.com>
2023-07-26 12:57:56 +0530

    style :  style fixed

--- src/frontend/components/common/SearchBar.jsx
+++ src/frontend/components/common/SearchBar.jsx
@@ -36,7 +36,7 @@ const SearchBar = () => {
         <div className='w-full  relative '>
             <input type="text" className='px-2 py-2 border w-full rounded text-lg outline-indigo-700 border-black' placeholder='Search Here' onChange={(e) => setSearchInput(e.target.value)} value={searchInput} />
             {showSearchOutputModal &&
-                <div className='flex max-h-[50vh] overflow-y-scroll flex-col gap-3 bg-white absolute w-full rounded-b-xl border-x border-b px-2   py-2  ' ref={searchRef}>
+                <div className='flex max-h-[50vh] overflow-y-scroll z-10 flex-col gap-3 bg-white absolute w-full rounded-b-xl border-x border-b px-2   py-2  ' ref={searchRef}>
                     {searchData.length === 0 ? (
                         <p className='text-center text-lg'>No item to show</p>
                     ) : (
@@ -51,7 +51,7 @@ const SearchBar = () => {
                                     className='flex justify-between items-center px-2 py-1 changeColorOnHover rounded border'
                                 >
                                     <img
-                                        className='w-10 h-auto filter mix-blend-multiply   '
+                                        className='w-10 h-auto  mix-blend-multiply   '
                                         loading="lazy"
                                         src={image}
                                         alt='nav search img'
